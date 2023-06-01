604ed55adee37313860291d14fa21faf57800e96
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-01 16:27:15 +0200

    menu

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const sidebarButton = document.getElementById('sidebar-button')
 const sidebar = document.getElementById('sidebar')
+const sidebarText = document.getElementById('sidebar-text')
 
 sidebarButton.addEventListener('click', () => {
   console.log('clicked');
-  sidebar.classList.toggle('hidden')
+  sidebar.classList.toggle('show')
 })
