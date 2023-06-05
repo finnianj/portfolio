2f58287639e0bff1f37c4dac0dd73f05c121d73a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-05 12:24:07 +0200

    js

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -5,13 +5,15 @@ const dateSubmit = document.getElementById("date-submit");
 dateSubmit.addEventListener('click', (e) => {
   e.preventDefault()
   console.log(dateInput.value);
+  window.open(`/api/date/${dateInput.value}`, '_blank');
+  dateInput.value = ""
 })
 
-const sidebarButton = document.getElementById('sidebar-button')
-const sidebar = document.getElementById('sidebar')
-// const sidebarText = document.getElementById('sidebar-text')
+// const sidebarButton = document.getElementById('sidebar-button')
+// const sidebar = document.getElementById('sidebar')
+// // const sidebarText = document.getElementById('sidebar-text')
 
-sidebarButton.addEventListener('click', () => {
-  console.log('clicked');
-  sidebar.classList.toggle('show')
-})
+// sidebarButton.addEventListener('click', () => {
+//   console.log('clicked');
+//   sidebar.classList.toggle('show')
+// })
