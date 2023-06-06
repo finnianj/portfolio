c2cb9c4c9cd6eadba6ca7a6e5eb630478a654993
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-06 17:17:57 +0200

    favourite song database

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -3,6 +3,31 @@ const dateSubmit = document.getElementById("date-submit");
 const converterInput = document.getElementById("converter-input");
 const converterSubmit = document.getElementById("converter-submit");
 
+const apiItemSong = document.getElementById("api-item-song");
+const apiItemDate = document.getElementById("api-item-date");
+const apiItemConverter = document.getElementById("api-item-converter");
+const apiItemIP = document.getElementById("api-item-ip");
+
+const linkSong = document.getElementById("link-to-song");
+const linkDate = document.getElementById("link-to-date");
+const linkConverter = document.getElementById("link-to-converter");
+const linkIP = document.getElementById("link-to-ip");
+
+linkSong.addEventListener('click', () => {
+  console.log('ok');
+  console.log(apiItemSong);
+  apiItemSong.scrollIntoView({behavior:"smooth"})
+})
+linkDate.addEventListener('click', () => {
+  apiItemDate.scrollIntoView({behavior:"smooth"})
+})
+linkConverter.addEventListener('click', () => {
+  apiItemConverter.scrollIntoView({behavior:"smooth"})
+})
+linkIP.addEventListener('click', () => {
+  apiItemIP.scrollIntoView({behavior:"smooth"})
+})
+
 
 dateSubmit.addEventListener('click', (e) => {
   e.preventDefault()
@@ -17,6 +42,8 @@ converterSubmit.addEventListener('click', (e) => {
   window.open(`/api/converter?value=${converterInput.value}`, '_blank');
   converterInput.value = ""
 })
+
+
 
 // const sidebarButton = document.getElementById('sidebar-button')
 // const sidebar = document.getElementById('sidebar')
