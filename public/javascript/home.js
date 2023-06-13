2b1308ed9e2e906c66fdac540033d8b2f48c9014
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-13 15:41:29 +0200

    info button

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,5 +1,6 @@
 const sidebarButton = document.getElementById('sidebar-button')
 const sidebar = document.getElementById('sidebar')
+const overlay = document.getElementById('overlay')
 // const sidebarText = document.getElementById('sidebar-text')
 const infoButtons = document.getElementsByClassName('info-wrapper')
 sidebarButton.addEventListener('click', () => {
@@ -9,6 +10,7 @@ sidebarButton.addEventListener('click', () => {
 
 Array.from(infoButtons).forEach(b => {
   b.children[0].addEventListener('click', (e) => {
-    b.children[1].classList.toggle('invisible');
+    overlay.classList.toggle('overlay-show')
+    b.children[1].classList.toggle('visible');
   })
 });
