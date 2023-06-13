640aa386d0fdbb1ab2b35eed6b670614376fcbdd
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-13 17:29:51 +0200

    info button

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -17,8 +17,8 @@ Array.from(infoButtons).forEach(b => {
 
 overlay.addEventListener('click', () => {
   console.log('clicked');
-  overlay.classList.remove('overlay-show')
+  overlay.classList.remove('visible')
   Array.from(infoButtons).forEach(b => {
-    b.children[1].classList.toggle('visible');
+    b.children[1].classList.remove('visible');
   });
 })
