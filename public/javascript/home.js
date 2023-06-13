975e091b19101fa1a64e18a81f4fa4603221d939
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-13 13:53:49 +0200

    info button

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,8 +1,15 @@
 const sidebarButton = document.getElementById('sidebar-button')
 const sidebar = document.getElementById('sidebar')
 // const sidebarText = document.getElementById('sidebar-text')
+const infoButtons = document.getElementsByClassName('fa-circle-info')
 
 sidebarButton.addEventListener('click', () => {
   console.log('clicked');
   sidebar.classList.toggle('show')
 })
+
+Array.from(infoButtons).forEach(b => {
+  b.addEventListener('click', (e) => {
+    e.currentTarget.children[0].classList.toggle('d-none');
+  })
+});
