827b99ac4f709576a9be3f74c836bf99c4dc7e34
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-15 15:48:36 +0200

    menu

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -45,40 +45,4 @@ converterSubmit.addEventListener('click', (e) => {
 
 
 
-// const sidebarButton = document.getElementById('sidebar-button')
-// const sidebar = document.getElementById('sidebar')
-// // const sidebarText = document.getElementById('sidebar-text')
 
-// sidebarButton.addEventListener('click', () => {
-//   console.log('clicked');
-//   sidebar.classList.toggle('show')
-// })
-
-const sidebarButton = document.getElementById('sidebar-button')
-const sidebar = document.getElementById('sidebar')
-const overlay = document.getElementById('overlay')
-const text = document.getElementById('homepage-text')
-
-// const sidebarText = document.getElementById('sidebar-text')
-const infoButtons = document.getElementsByClassName('info-wrapper')
-sidebarButton.addEventListener('click', () => {
-  console.log('clicked');
-  sidebar.classList.toggle('show')
-})
-
-Array.from(infoButtons).forEach(b => {
-  b.children[0].addEventListener('click', (e) => {
-    text.classList.toggle('text-fade')
-    overlay.classList.toggle('visible')
-    b.children[1].classList.toggle('visible');
-  })
-});
-
-overlay.addEventListener('click', () => {
-  console.log('clicked');
-  text.classList.add('text-fade')
-  overlay.classList.remove('visible')
-  Array.from(infoButtons).forEach(b => {
-    b.children[1].classList.remove('visible');
-  });
-})
