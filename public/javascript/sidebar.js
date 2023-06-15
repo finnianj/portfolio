25884e9dde7487248c31c38609c3ea5f9979dc5d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-15 17:32:42 +0200

    menu

--- public/javascript/sidebar.js
+++ public/javascript/sidebar.js
@@ -2,6 +2,7 @@ export function sidebar() {
   const sidebarButton = document.getElementById('sidebar-button')
   const sidebar = document.getElementById('sidebar')
   const sidebarItems = document.getElementsByClassName('sidebar-item')
+  const text = document.getElementById('homepage-text')
 
   sidebarButton.addEventListener('click', () => {
     sidebar.classList.toggle('show')
@@ -12,7 +13,8 @@ export function sidebar() {
     const name = e.currentTarget.classList[1].split('-')[1];
     const target = document.querySelector(`#${name}`)
     console.log(target);
-    target.scrollIntoView({
+    text.scrollTo({
+      top: target.getBoundingClientRect().top - 200,
       behaviour: 'smooth'
     })
   }
