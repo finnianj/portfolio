7c6acb9a96e1a646e2d593c5ffe174dd64eec6c4
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-18 11:14:33 +0200

    epic function import/expor

--- public/javascript/sidebar.js
+++ public/javascript/sidebar.js
@@ -1,33 +1,13 @@
 import { scroll } from './scroll.js'
 
-export function sidebar() {
+export function sidebar(textContainer) {
   const sidebarButton = document.getElementById('sidebar-button')
   const sidebar = document.getElementById('sidebar')
   const sidebarLinks = document.getElementsByClassName('sidebar-link')
-  const text = document.getElementById('homepage-text')
 
   sidebarButton.addEventListener('click', () => {
     sidebar.classList.toggle('show')
   })
 
-  scroll(sidebarLinks, text, sidebar)
-
-  // const handleClick = function(e) {
-  //   e.preventDefault()
-  //   sidebar.classList.toggle('show')
-  //   const name = e.currentTarget.classList[1].split('-')[1];
-  //   const target = document.querySelector(`#${name}`)
-  //   text.scrollTo({
-  //     top: text.scrollTop + target.getBoundingClientRect().top - 100,
-  //     behaviour: 'smooth'
-  //   })
-
-  // }
-
-  // Array.from(sidebarItems).forEach(i => {
-  //   i.addEventListener('click', handleClick)
-  // });
-
-
-
+  scroll(sidebarLinks, textContainer, sidebar)
 }
