1e308cec2eccca01c0092be8aae90f3dc3caaab0
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-06-18 10:43:15 +0200

    streamlining

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,34 +1,34 @@
-import { sidebar } from "./sidebar.js";
+// import { sidebar } from "./sidebar.js";
 
 const dateInput = document.getElementById("date-input");
 const dateSubmit = document.getElementById("date-submit");
 const converterInput = document.getElementById("converter-input");
 const converterSubmit = document.getElementById("converter-submit");
 
-const apiItemSong = document.getElementById("api-item-song");
-const apiItemDate = document.getElementById("api-item-date");
-const apiItemConverter = document.getElementById("api-item-converter");
-const apiItemIP = document.getElementById("api-item-ip");
+// const apiItemSong = document.getElementById("api-item-song");
+// const apiItemDate = document.getElementById("api-item-date");
+// const apiItemConverter = document.getElementById("api-item-converter");
+// const apiItemIP = document.getElementById("api-item-ip");
 
-const linkSong = document.getElementById("link-to-song");
-const linkDate = document.getElementById("link-to-date");
-const linkConverter = document.getElementById("link-to-converter");
-const linkIP = document.getElementById("link-to-ip");
+// const linkSong = document.getElementById("link-to-song");
+// const linkDate = document.getElementById("link-to-date");
+// const linkConverter = document.getElementById("link-to-converter");
+// const linkIP = document.getElementById("link-to-ip");
 
-linkSong.addEventListener('click', () => {
-  console.log('ok');
-  console.log(apiItemSong);
-  apiItemSong.scrollIntoView({behavior:"smooth"})
-})
-linkDate.addEventListener('click', () => {
-  apiItemDate.scrollIntoView({behavior:"smooth"})
-})
-linkConverter.addEventListener('click', () => {
-  apiItemConverter.scrollIntoView({behavior:"smooth"})
-})
-linkIP.addEventListener('click', () => {
-  apiItemIP.scrollIntoView({behavior:"smooth"})
-})
+// linkSong.addEventListener('click', () => {
+//   console.log('ok');
+//   console.log(apiItemSong);
+//   apiItemSong.scrollIntoView({behavior:"smooth"})
+// })
+// linkDate.addEventListener('click', () => {
+//   apiItemDate.scrollIntoView({behavior:"smooth"})
+// })
+// linkConverter.addEventListener('click', () => {
+//   apiItemConverter.scrollIntoView({behavior:"smooth"})
+// })
+// linkIP.addEventListener('click', () => {
+//   apiItemIP.scrollIntoView({behavior:"smooth"})
+// })
 
 
 dateSubmit.addEventListener('click', (e) => {
@@ -45,4 +45,23 @@ converterSubmit.addEventListener('click', (e) => {
   converterInput.value = ""
 })
 
-sidebar()
+// sidebar()
+
+
+const apiItems = document.getElementsByClassName('api-item')
+
+
+const handleAPIClick = function(e) {
+  e.preventDefault()
+  const name = e.currentTarget.classList[1].split('-')[2];
+  const target = document.querySelector(`#${name}`)
+  text.scrollTo({
+    top: text.scrollTop + target.getBoundingClientRect().top - 100,
+    behaviour: 'smooth'
+  })
+
+}
+
+Array.from(apiItems).forEach(i => {
+  i.addEventListener('click', handleAPIClick)
+});
